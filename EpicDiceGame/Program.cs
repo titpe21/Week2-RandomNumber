3c152a904fdc7475f0158540b1e12d099e80b6f6
Julia Aasmaa <my_e_10@hotmail.com>
2021-09-08 12:30:49 +0300

    add random

---
+++ EpicDiceGame/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EpicDiceGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ RandomNumberGenerators/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace RandomNumberGenerators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+
+            int MyRandomNumber = rnd.Next(1, 11);
+
+            //programm genereerib juhuslikku numbrit 1 kuni 10;
+            //programm kontrollib, kas genereeritud number on
+            //suurem, kui 5;
+            //kui number on suurem, kui 5, siis konsool kuvab
+            //"juhuslik number on {MyRandomNumer}, see on suurem,
+            //kui 5";
+            //kui number on väiksem, kui 5, konsool kuvab
+            //juhuslik number on {MyRandomNumer}, see on väiksem,
+            //kui 5";
+            //kuinumber on 5, siis konsool kuvab "juhuslik number on 5"
+
+
+            Console.WriteLine($"Arvuti genereeris: {MyRandomNumber}");
+
+            if(MyRandomNumber > 5)
+            {
+                Console.WriteLine($"juhuslik number on {MyRandomNumber}, see on suurem, kui 5");
+            }
+            else if (MyRandomNumber < 5)
+            {
+                Console.WriteLine($"juhuslik number on {MyRandomNumber}, see on väiksem, kui 5");
+            }
+            else
+            {
+                Console.WriteLine("Juhuslik number ongi 5");
+            }
+
+
+
+        }
+    }
+}
